30fcfeb6dc72f90878c719e2b5972a5c2da9a10e
conglinyizhi <conglinyizhi@qq.com>
2026-05-24 10:02:14 +0800

    Use small shipyard item in creative tab

--- src/main/java/org/trp/shincolle/init/ModTabs.java
+++ src/main/java/org/trp/shincolle/init/ModTabs.java
@@ -102,7 +102,7 @@ public class ModTabs {
                 output.accept(ModItems.POLYMETAL.get());
                 output.accept(ModItems.POLYMETAL_ORE.get());
                 output.accept(ModItems.POLYMETAL_GRAVEL.get());
-                output.accept(ModBlocks.SMALL_SHIPYARD.get());
+                output.accept(ModItems.SMALL_SHIPYARD.get());
                 output.accept(ModItems.LARGE_SHIPYARD.get());
                 output.accept(ModItems.DESK.get());
                 output.accept(ModItems.VOL_CORE.get());
